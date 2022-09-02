1ae6d654fa1539617acfbd3da6781871fbcdfa38
Alexey Razmanov <razmanov666@gmail.com>
2022-09-02 20:14:19 +0300

    add field user from AUTH_USER_MODEL

--- drf/support/models.py
+++ drf/support/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -5,7 +6,10 @@ class Ticket(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     is_solved = models.BooleanField(default=False)
-    # user_id = models.ForeignKey()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return self.title
