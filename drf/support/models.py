f353ac37437c1553395b665152f1893ef47400cc
Alexey Razmanov <razmanov666@gmail.com>
2022-09-02 14:44:21 +0300

    create demo model

--- drf/support/models.py
+++ drf/support/models.py
@@ -1,2 +1,11 @@
-# from django.db import models
-# Create your models here.
+from django.db import models
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    is_solved = models.BooleanField(default=False)
+    # user_id = models.ForeignKey()
+
+    def __str__(self):
+        return self.title
