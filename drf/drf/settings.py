2133c0749db6eb9d71493389ddf0916a548f8710
Alexey Razmanov <razmanov666@gmail.com>
2022-09-02 14:43:20 +0300

    add apps & connect postgre

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "support.apps.SupportConfig",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -74,10 +76,21 @@ WSGI_APPLICATION = "drf.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "support_db",
+        "USER": "my_admin_user",
+        "PASSWORD": "ASDFGHJKl99",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
