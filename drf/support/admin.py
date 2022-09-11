a357fe994defa83433df84b45dbc3c0f821309ef
Alexey Razmanov <razmanov666@gmail.com>
2022-09-11 18:57:54 +0300

    delete app support

--- drf/support/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Comment
-from .models import Status
-from .models import Ticket
-
-admin.site.register(Ticket)
-admin.site.register(Comment)
-admin.site.register(Status)
