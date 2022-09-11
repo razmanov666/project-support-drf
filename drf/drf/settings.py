6fd37d50c167373a40589af8efd06a28d39b6ba3
Alexey Razmanov <razmanov666@gmail.com>
2022-09-11 18:27:19 +0300

    init apps Ticket and Comment

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "support.apps.SupportConfig",
+    "comment.apps.CommentConfig",
     "rest_framework",
 ]
 
