55d50757826328b3aa5d79eb2e8f0e91e690a503
razmanov666 <razmanov666@gmail.com>
2022-09-17 03:55:00 +0300

    set dependence for creating comment from ticket

--- drf/comment/serializers.py
+++ drf/comment/serializers.py
@@ -1,12 +1,33 @@
 from rest_framework import serializers
 
-from .models import Comment
+from .models import Comment, Ticket
+
+
+class CurrentTicketDefault():
+    requires_context = True
+
+    def __call__(self, serializer_field):
+        # print(serializer_field.context['request'].build_absolute_uri().split('/'))
+        return Ticket.objects.get(pk=serializer_field.context['request'].build_absolute_uri().split('/')[5])
+
+    def __repr__(self):
+        return '%s()' % self.__class__.__name__
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = serializers.ReadOnlyField(source="user.username")
-    ticket = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    ticket = serializers.HiddenField(default=CurrentTicketDefault())
 
     class Meta:
         model = Comment
         fields = "__all__"
+
+    def create(self, validated_data):
+        # self.ticket = Ticket.objects.get(pk=validated_data['ticket'].pk)
+        comment = Comment(
+            ticket=validated_data['ticket'],
+            user=validated_data['user'],
+            text=validated_data['text']
+        )
+        comment.save()
+        return comment
