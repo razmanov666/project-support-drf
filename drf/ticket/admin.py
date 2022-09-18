0d0352dabfb97c62ed5b52440179534db32923be
Alexey Razmanov <razmanov666@gmail.com>
2022-09-18 19:37:34 +0300

    set admin shielding for ticket

--- drf/ticket/admin.py
+++ drf/ticket/admin.py
@@ -3,5 +3,21 @@ from django.contrib import admin
 from .models import Status
 from .models import Ticket
 
-admin.site.register(Ticket)
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "text",
+        "status",
+        "time_create",
+        "time_update",
+        "user",
+    )
+    # list_display_links = ("id", "title")
+    # search_fields = ("title", "content")
+    # list_editable = ("is_published",)
+    # list_filter = ("is_published", "category")
+
+
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(Status)
