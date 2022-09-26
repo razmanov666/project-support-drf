a3b89bc827be3395a3ba5acb6ffd8f2b69a4d579
razmanov666 <razmanov666@gmail.com>
2022-09-26 16:40:33 +0300

    authentickated user cannot register new user

--- drf/userauth/views.py
+++ drf/userauth/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
 
 from .models import CustomUser
+from .permissions import IsNotAuthenticated
 from .serializers import RegisterSerializer
 
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
-    permission_classes = (AllowAny,)
+    permission_classes = (IsNotAuthenticated,)
     serializer_class = RegisterSerializer
