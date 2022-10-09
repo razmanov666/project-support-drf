3da4fca45f7624f333d8515e5c36d3b8255df37d
razmanov666 <razmanov666@gmail.com>
2022-10-09 14:20:43 +0300

    try to create task for autofrozen

--- support/comment/tasks.py
+++ support/comment/tasks.py
@@ -1,12 +1,14 @@
 # from django.core.mail import send_mail
-from support.celery import app
-
-
-@app.task
-def say_work(arg):
-    print("work " + arg)
-
-
+# from ticket.models import Ticket
+# from .models import Comment
+# from support.celery import app
+# @app.task
+# def autofrozen():
+#     non_frozen_objects = Ticket.objects.filter(status_status != "Frozen")
+#     print(non_frozen_objects)
+# @app.task
+# def say_work(arg):
+#     print("work " + arg)
 # @app.task
 # def send_email():
 #     send_mail(
--- support/support/celery.py
+++ support/support/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "support.settings")
 
@@ -10,3 +11,10 @@ app = Celery("support")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_shedule = {
+    "check-ticket-for-autofrozen": {
+        "task": "comment.tasks.autofrozen",
+        "schedule": crontab(minute="*/5"),
+    }
+}
