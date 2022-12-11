6d17746812f8010e069d2eaa569b33f62e85216b
razmanov666 <razmanov666@gmail.com>
2022-12-11 21:00:08 +0300

    refactor email_name && manager_list_mail

--- support/ticket/service.py
+++ support/ticket/service.py
@@ -1,32 +1,42 @@
-from django.core.mail import send_mail
 import json
+import os
+from pathlib import Path
+
+import environ
+from django.core.mail import send_mail
 from userauth.models import CustomUser
+
+env = environ.Env(DEBUG=(bool, False))
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR.parent, ".env"))
+
 
 def send_mail_update_comments(object):
     message_data = json.loads(object)
-    title_message = 'Ticket "' + message_data.get('ticket') + '" has update.'
+    title_message = 'Ticket "' + message_data.get("ticket") + '" has update.'
     text_message = "Dear, " + message_data.get("username") + " you are have a new message in your ticket"
     send_mail(
-                title_message,
-                text_message,
-                "app_notification_project@mail.ru",
-                [message_data.get("email")],
-                fail_silently=False,
-            )
-    
+        title_message,
+        text_message,
+        env("EMAIL_HOST_USER"),
+        [message_data.get("email")],
+        fail_silently=False,
+    )
 
 
 def send_mail_tickets_without_assigned(count: int):
-    # message_data = json.loads(object)
-    manager_list = CustomUser.objects.filter(role="MG")
-    manager_list_mail = [user.email for user in manager_list]
+    manager_list_mail = [user.email for user in CustomUser.objects.filter(role="MG")]
     title_message = str(count) + " Tickets haven't assigned manager."
     text_message = "http://127.0.0.1:8000/api/tickets/opened/"
     send_mail(
-                title_message,
-                text_message,
-                "app_notification_project@mail.ru",
-                manager_list_mail,
-                fail_silently=False,
-            )
-    
+        title_message,
+        text_message,
+        env("EMAIL_HOST_USER"),
+        manager_list_mail,
+        fail_silently=False,
+    )
+
+
+def send_mail_change_status_of_ticket():
+    pass
